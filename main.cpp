6c35813f15ce69c199becec20bfee5cdd76198f4
Filip Procházka <filip@prochazka.su>
2016-11-30 15:07:58 +0100

    KICKASS

--- main.cpp
+++ main.cpp
@@ -7,15 +7,37 @@
 #include <cassert>
 #include <queue>
 #include <ctime>
+#include <cmath>
+
+#define DEBUG 0
+#define DEBUG_HEAT 1
+#define DEBUG_DIAGONAL 1
+#define SYMBOL_X 'X'
+#define SYMBOL_O 'O'
+#define SYMBOL_BOUND 'B'
+#define SYMBOL_EMPTY '-'
 
 using Coordinate = unsigned;
-using Coordinates = std::pair<Coordinate, Coordinate>;
 using Symbol = char;
+using Diagonal = std::array<Symbol, 9>;
 
-unsigned random(unsigned max) {
+class Coordinates {
+public:
+	Coordinate x;
+	Coordinate y;
+	Coordinates(Coordinate x, Coordinate y) : x(x), y(y) {}
+	Coordinates() {}
+};
+
+//unsigned numberOfDigits(int n) {
+//	n = abs(n);
+//	return (unsigned) (n > 0 ? (int) log10 ((double) n) + 1 : 1);
+//}
+
+unsigned random(unsigned from, unsigned to) {
 	std::random_device device;
 	std::mt19937 generator(device());
-	std::uniform_int_distribution<> distribution(0, max);
+	std::uniform_int_distribution<> distribution(from, to);
 	return (unsigned int) distribution(generator );
 }
 
@@ -23,188 +45,304 @@ class TicTacToe {
 
 	Coordinate boardSize;
 	std::vector<char> board;
+	std::vector<bool> boardCheckPositions;
 	Symbol ourSymbol;
 	Symbol enemySymbol;
+
+	int checkDirections[8][2] = {
+		{-1, 0}, // vlevo
+		{-1, -1}, // vlevo nahoru
+		{0, -1}, // nad
+		{1, -1}, // vpravo nahoru
+		{1, 0}, // vpravo
+		{1, 1}, // vpravo dolu
+		{0, 1}, // dolu
+		{-1, 1} // vlevo dolu
+	};
 
 public:
 	void run(std::istream &in, std::ostream &out, std::ostream &err) {
 		in >> boardSize;
 		initPlayground();
 
-		in >> ourSymbol;
-		enemySymbol = ourSymbol == 'X' ? 'O' : 'X';
+		do {
+			in >> ourSymbol;
+			if (ourSymbol != SYMBOL_X && ourSymbol != SYMBOL_O) {
+				err << "Spatny symbol pro Skynet AI, dovolene jsou pouze X a O ... zkus to znova a nezapomen na SHIFT!" << std::endl;
+				continue;
+			}
+
+			enemySymbol = ourSymbol == SYMBOL_X ? SYMBOL_O : SYMBOL_X;
+			break;
+
+		} while (true);
 
 		Coordinates ourPlacement;
-		int x, y;
+		int playerX, playerY;
 		while (true) {
-			if (ourSymbol == 'X') {
-				ourPlacement = computerPlace();
-				out << ourPlacement.first << " " << ourPlacement.second << std::endl;
-				debugPlayground(out);
+			if (ourSymbol == SYMBOL_X) {
+				ourPlacement = computerPlace(out, err);
+				out << ourPlacement.x << " " << ourPlacement.y << std::endl;
+				if (DEBUG) {
+					debugPlayground(out, board);
+				}
 			}
 
 			do {
-				in >> x;
-				in >> y;
-				if (x == -1 && y == -1) {
+				in >> playerX;
+				in >> playerY;
+				if (playerX == -1 && playerY == -1) {
 					return; // end
 				}
-			} while (!enemyPlace((Coordinate) x, (Coordinate) y, err));
+			} while (!enemyPlace({(Coordinate) playerX, (Coordinate) playerY}, err));
 
-			if (ourSymbol == 'O') {
-				ourPlacement = computerPlace();
-				out << ourPlacement.first << " " << ourPlacement.second << std::endl;
-				debugPlayground(out);
+			if (ourSymbol == SYMBOL_O) {
+				ourPlacement = computerPlace(out, err);
+				out << ourPlacement.x << " " << ourPlacement.y << std::endl;
+				if (DEBUG) {
+					debugPlayground(out, board);
+				}
 			}
 		}
 	}
 
-	bool enemyPlace(Coordinate x, Coordinate y, std::ostream &err) {
-		if (x > boardSize - 1 || y > boardSize - 1) {
+	bool enemyPlace(const Coordinates &point, std::ostream &err) {
+		if (point.x > boardSize - 1 || point.y > boardSize - 1) {
 			err << "out of bounds: fuck off" << std::endl;
-			return 0;
+			return false;
 		}
 
-		if (board[positionFromCoordinates(x, y)] != '-') {
-			err << "uz tam neco je vole: "<< board[positionFromCoordinates(x, y)] << std::endl;
-			return 0;
+		if (!placeSymbol(enemySymbol, point)) {
+			err << "uz tam neco je vole: "<< board[positionFromCoordinates(point)] << std::endl;
+			return false;
 		}
 
-		board[positionFromCoordinates(x, y)] = enemySymbol;
-		return 1;
+		return true;
 	}
 
-	Coordinates computerPlace() {
+	Coordinates computerPlace(std::ostream &out, std::ostream &err) {
 		Coordinates finalPlacement;
 
-		int checkDirections[8][2] = {
-			{-1, -1}, // vlevo nahoru
-			{0, -1}, // nad
-			{1, -1}, // vpravo nahoru
-			{1, 0}, // vpravo
-			{1, 1}, // vpravo dolu
-			{0, 1}, // dolu
-			{-1, 1}, // vlevo dolu
-			{-1, 0} // vlevo
-		};
-
-		int maxDanger = 0;
-		Coordinates maxDangerPosition;
-		std::vector<unsigned> dangerLevels(boardSize * boardSize);
-
-		int maxAttack = 0;
-		Coordinates maxAttackPosition;
-		std::vector<unsigned> attackLevels(boardSize * boardSize);
+		int maxHeat = 0;
+		Coordinates maxHeatPosition;
+		std::vector<unsigned> heatLevels(boardSize * boardSize);
 
 		for (Coordinate y = 0; y < boardSize ;y++) {
 			for (Coordinate x = 0; x < boardSize ;x++) {
-				if (board[positionFromCoordinates(x, y)] == enemySymbol) {
-					for (size_t d = 0;d < 8; d++) {
-						Coordinate dx = x;
-						Coordinate dy = y;
-						for (size_t change = 1; change <= 4; change++) {
-							if ((int) (dx + checkDirections[d][0]) < 0 || (int) (dx + checkDirections[d][0]) > ((int) boardSize - 1)) {
-								break; // bound
-							}
-							if ((int) (dy + checkDirections[d][1]) < 0 || (int) (dy + checkDirections[d][1]) > ((int) boardSize - 1)) {
-								break; // bound
-							}
+				Coordinates originCursor(x, y);
+				Coordinate originCursorPosition = positionFromCoordinates(originCursor);
 
-							dx += checkDirections[d][0];
-							dy += checkDirections[d][1];
+				if (!boardCheckPositions[originCursorPosition]) {
+					heatLevels[originCursorPosition] = 0;
+					continue; // ignore
+				}
 
-							Coordinate position = positionFromCoordinates(dx, dy);
-							if (board[position] == ourSymbol) {
-								break;
-							}
-							if (board[position] == '-') {
-								dangerLevels[position] += change;
-								if ((int) dangerLevels[position] > maxDanger) {
-									maxDanger = dangerLevels[position];
-									maxDangerPosition = {dx, dy};
-								}
-								break;
-							}
+
+				if (board[originCursorPosition] != SYMBOL_EMPTY) {
+					if (DEBUG || DEBUG_HEAT) {
+						err << "Queue na kontrolu obsahuje neprazdne policko: " << originCursor.x << "x" << originCursor.y << std::endl;
+					}
+					continue;
+				}
+
+				for (size_t dir = 0; dir < 4; dir++) { // intentionally only 4 !!!
+//					if (DEBUG_DIAGONAL) {
+//						std::vector<char> diagonalSelectionBoard = board; // copy
+//					}
+
+					Diagonal diagonal;
+					Coordinates diffCursor(
+						originCursor.x + checkDirections[dir][0] * 5,
+						originCursor.y + checkDirections[dir][1] * 5
+					);
+					for (int i = 0; i < 9; i++) {
+						diffCursor.x += checkDirections[dir][0] * -1;
+						diffCursor.y += checkDirections[dir][1] * -1;
+						if (!isValidCoordinates(diffCursor.x, diffCursor.y)) {
+							diagonal[i] = SYMBOL_BOUND;
+							continue;
 						}
+
+						Coordinate diagonalPosition = positionFromCoordinates(diffCursor);
+						diagonal[i] = board[diagonalPosition];
+//						if (DEBUG_DIAGONAL) {
+//							diagonalSelectionBoard[diagonalPosition] = (i == 4) ? 'M' : ('0' + i);
+//						}
 					}
 
-				} else if (board[positionFromCoordinates(x, y)] == ourSymbol) {
-					for (size_t d = 0;d < 8; d++) {
-						Coordinate dx = x;
-						Coordinate dy = y;
-						for (size_t change = 1; change <= 4; change++) {
-							if ((int) (dx + checkDirections[d][0]) < 0 || (int) (dx + checkDirections[d][0]) > ((int) boardSize - 1)) {
-								break; // bound
-							}
-							if ((int) (dy + checkDirections[d][1]) < 0 || (int) (dy + checkDirections[d][1]) > ((int) boardSize - 1)) {
-								break; // bound
-							}
+//					if (DEBUG_DIAGONAL) {
+//						debugPlayground(out, diagonalSelectionBoard);
+//					}
 
-							dx += checkDirections[d][0];
-							dy += checkDirections[d][1];
+					size_t change = calculateDiagonalHeat(diagonal);
 
-							Coordinate position = positionFromCoordinates(dx, dy);
-							if (board[position] == enemySymbol) {
-								break;
-							}
-							if (board[position] == '-') {
-								attackLevels[position] += change > 3 ? 1337 : change;
-								if ((int) attackLevels[position] > maxAttack) {
-									maxAttack = attackLevels[position];
-									maxAttackPosition = {dx, dy};
-								}
-								break;
-							}
-						}
+					heatLevels[originCursorPosition] += change;
+					if ((int) heatLevels[originCursorPosition] > maxHeat) {
+						maxHeat = heatLevels[originCursorPosition];
+						maxHeatPosition = originCursor;
 					}
 				}
 			}
 		}
 
-//		if (maxAttack > 0 && (maxAttack - maxDanger > 2 || maxDanger < 2)) {
-//			board[positionFromCoordinates(maxAttackPosition.first, maxAttackPosition.second)] = ourSymbol;
-//			return maxAttackPosition;
-//		}
+		if (maxHeat > 0) {
+			finalPlacement = maxHeatPosition;
+			placeSymbol(ourSymbol, finalPlacement);
 
-		if (maxDanger > 1) {
-			board[positionFromCoordinates(maxDangerPosition.first, maxDangerPosition.second)] = ourSymbol;
-			return maxDangerPosition;
+		} else {
+			do {
+				int middle = (boardSize - 1) / 2;
+				finalPlacement = {
+					random(middle - (middle /2), middle + (middle /2)),
+					random(middle - (middle /2), middle + (middle /2))
+				};
+				if (placeSymbol(ourSymbol, finalPlacement)) {
+					break;
+				}
+
+			} while(true);
 		}
 
-//		finalPlacement = {(boardSize - 1) / 2, (boardSize - 1) / 2};
-//		board[positionFromCoordinates(finalPlacement.first, finalPlacement.second)] = ourSymbol;
-
-		do {
-			finalPlacement = {random(boardSize - 1), random(boardSize - 1)};
-			Coordinate position = positionFromCoordinates(finalPlacement.first, finalPlacement.second);
-			if (board[position] == '-') {
-				board[position] = ourSymbol;
-				break;
-			}
-
-		} while(true);
+		if (DEBUG_HEAT) {
+			debugHeatLevels(out, finalPlacement, heatLevels);
+		}
 
 		return finalPlacement;
 	}
 
-	Coordinate positionFromCoordinates(Coordinate x, Coordinate y) {
-		return x + (y * boardSize);
+	size_t calculateDiagonalHeat(const Diagonal &diagonal) const {
+		return std::max<size_t>(
+			calculateDiagonalHeatForSymbol(ourSymbol, diagonal)
+			+ calculateDiagonalHeatForSymbol(enemySymbol, diagonal),
+		    0
+		);
 	}
 
-	void debugPlayground(std::ostream &out) {
+	size_t calculateDiagonalHeatForSymbol(Symbol symbol, const Diagonal &diagonal) const {
+		size_t heat = 0;
+		size_t playableFields = 1; // middle
+		bool symbolsInRow = true; // middle
+		Symbol opositeSymbol = symbol == SYMBOL_O ? SYMBOL_X :SYMBOL_O;
+
+		for (int i = -1; i <= 1 ; i += 2) { // both directions from position 4
+			for (int j = 4 + i; j > 0 && j < 9 ;j += i) {
+				if (diagonal[j] == SYMBOL_BOUND || diagonal[j] == opositeSymbol) {
+					break; // nowhere to play here
+				}
+
+				if (diagonal[j] == SYMBOL_EMPTY) {
+					symbolsInRow = false;
+				}
+
+				playableFields++; // our or empty
+				if (symbolsInRow) {
+					heat += pow(2, i > 0 ? (j - 4) : (4 - j));
+				}
+			}
+		}
+
+		return playableFields >= 5 ? heat : 0;
+	}
+
+	bool placeSymbol(char symbol, const Coordinates &point) {
+		Coordinate placementPosition = positionFromCoordinates(point);
+		if (board[placementPosition] != SYMBOL_EMPTY) {
+			return false;
+		}
+
+		// place symbol
+		board[placementPosition] = symbol;
+
+		// remove placement position from queue
+		boardCheckPositions[placementPosition] = false;
+
+		// queue neighbour fields
+		for (size_t dir = 0; dir < 8; dir++) {
+			int dx = point.x + checkDirections[dir][0];
+			int dy = point.y + checkDirections[dir][1];
+			if (!isValidCoordinates(dx, dx)) {
+				continue;
+			}
+
+			Coordinate diffCursorPosition = positionFromCoordinates({(Coordinate)dx, (Coordinate)dy});
+			if (board[diffCursorPosition] == SYMBOL_EMPTY) {
+				boardCheckPositions[diffCursorPosition] = true;
+			}
+		}
+
+		return true;
+	}
+
+	Coordinate positionFromCoordinates(Coordinates point) {
+		return point.x + (point.y * boardSize);
+	}
+
+	Coordinates coordinatesFromPosition(Coordinate position) {
+		return {
+			(position % boardSize) + 1,
+			(position / boardSize) + 1,
+		};
+	}
+
+	int moveVectorCoordinate(int coordinate, int direction, int change = 1) {
+		return coordinate + (direction * change) * (change < 0 ? -1 : 1);
+	}
+
+	bool isValidCoordinates(int x, int y) {
+		return !(x < 0 || x > ((int) boardSize) - 1)
+			&& !(y < 0 || y > ((int) boardSize) - 1);
+	}
+
+	void debugPlayground(std::ostream &out, const std::vector<char> boardData) {
+		out << " ";
+		for (Coordinate x = 0; x < boardSize ;x++) {
+			out << " " << x % 10;
+		}
+		out << std::endl;
 		for (Coordinate y = 0; y < boardSize ;y++) {
+			out << y % 10 << " ";
 			for (Coordinate x = 0; x < boardSize ;x++) {
-				out << board[positionFromCoordinates(x, y)] << " ";
+				out << boardData[positionFromCoordinates({x, y})] << " ";
 			}
 			out << std::endl;
 		}
+		out << std::endl;
+	}
+
+	void debugHeatLevels(std::ostream &out, const Coordinates &maxHeatPosition, const std::vector<unsigned> &heatLevels) {
+		Coordinate boardFields = boardSize * boardSize;
+		std::vector<char> boardCopy(boardFields);
+
+		for (Coordinate position = 0; position < (boardFields) ;position++) {
+			if (board[position] != SYMBOL_EMPTY || heatLevels[position] == 0) {
+				boardCopy[position] = board[position];
+				continue;
+			}
+
+			boardCopy[position] = heatLevels[position] > 9 ? '9' : ('0' + heatLevels[position]);
+		}
+
+		boardCopy[positionFromCoordinates(maxHeatPosition)] = '#';
+		debugPlayground(out, boardCopy);
+
+		for (Coordinate position = 0; position < (boardFields) ;position++) {
+			boardCopy[position] = boardCheckPositions[position] ? '?' : board[position];
+		}
+		debugPlayground(out, boardCopy);
 	}
 
 	void initPlayground() {
 		board = std::vector<char>(boardSize * boardSize);
 		for (Coordinate y = 0; y < boardSize ;y++) {
 			for (Coordinate x = 0; x < boardSize ;x++) {
-				board[positionFromCoordinates(x, y)] = '-';
+				board[positionFromCoordinates({x, y})] = SYMBOL_EMPTY;
+			}
+		}
+
+		boardCheckPositions = std::vector<bool>(boardSize * boardSize);
+		for (Coordinate y = 0; y < boardSize ;y++) {
+			for (Coordinate x = 0; x < boardSize ;x++) {
+				boardCheckPositions[positionFromCoordinates({x, y})] = false;
 			}
 		}
 	}
